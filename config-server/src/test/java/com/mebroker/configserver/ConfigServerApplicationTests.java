15fa04377b3fe7308e3fe6d6cc59021a668f9acb
Ahmed Fayez Ahmed <a.fayez@qad.qa>
2025-12-30 10:41:08 +0300

    first commit

---
+++ config-server/src/test/java/com/mebroker/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mebroker.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
